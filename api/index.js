0d4557a1cdf7115665380031b432229c52ac20ca
Yuzu <Yuzu_0204@outlook.com>
2024-12-15 19:46:28 -0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const app = express();
 app.use(
   cors({
-    origin: ["http://localhost:5173", "Frontend URL Vercel"],
+    origin: ["http://localhost:5173", "Frontend URL Vercel","http://localhost:4173"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
